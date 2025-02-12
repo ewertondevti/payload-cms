c56eb0787113ebaac36892471d9516c7eb016694
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2025-02-12 16:57:19 +0000

    identification

---
+++ src/app/(frontend)/[locale]/identificacao/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ApplicantIdentification } from '@/components/ApplicantIdentification';
+
+const Identificacao = () => {
+ return (<div>
+  <ApplicantIdentification />
+ </div>)
+}
+
+export default Identificacao;
---
+++ src/components/Link/ApplicantIdentification/index.tsx
@@ -0,0 +1,29 @@
+import configPromise from '@payload-config'
+import { notFound } from 'next/navigation'
+import { getPayload } from 'payload'
+import { ApplicantIdentificationForm } from './ApplicantIdentificationForm'
+
+export async function ApplicantIdentification() {
+ const payload = await getPayload({
+  config: configPromise,
+ })
+
+ const data = await payload.find({
+  collection: 'forms',
+  draft: false,
+  limit: 1,
+  overrideAccess: false,
+  where: {
+   id: {
+    equals: 1,
+   },
+  },
+ })
+
+
+ const currForm = data?.docs?.[0]
+
+ if (!currForm) return notFound()
+
+ return <ApplicantIdentificationForm form={currForm} />
+}
