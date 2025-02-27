05faabd0d09a1188497deef0ae58c4c04644649b
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2025-02-27 09:39:42 +0000

    up

---
+++ src/blocks/Form/DeathCertificate/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { AddressData } from '../AddressData';
+import { useForm } from 'react-hook-form';
+import FormTitle from '../formTitle';
+
+type DeathCertificateProps = {
+ title: string;
+ subtitle?: string;
+};
+const DeathCertificate: React.FC<DeathCertificateProps> = ({
+ title,
+ subtitle,
+}) => {
+ const { register, formState: { errors } } = useForm();
+
+ return (
+  <div>
+   <FormTitle title={title} subtitle={subtitle} />
+
+   {/* <AddressData name="address" register={register} errors={errors} /> */}
+
+
+  </div>
+ );
+};
+
+export default DeathCertificate;
